c8a5b3ec5c40c8953a3d8581cd8ddcd640a67acd
Chandra Sekhar Mullu <47244469+iamknownstranger@users.noreply.github.com>
2021-06-21 11:34:41 +0000

    EOD 21st June

--- Screeners/pre_open_arbitrage.py
+++ Screeners/pre_open_arbitrage.py
@@ -12,17 +12,22 @@ common_instruments = pd.read_csv("Screeners/common_instruments.csv")
 watchlist = []
 
 for index, row in common_instruments.iterrows():
- 
-    ltp_nse = kite.ltp(row['nse_instrument_token'])[str(row['nse_instrument_token'])]['last_price']
-    ltp_bse = kite.ltp(row['bse_instrument_token'])[str(row['bse_instrument_token'])]['last_price']
+    try:
+        ltp_nse = kite.ltp(row['nse_instrument_token'])[str(row['nse_instrument_token'])]['last_price']
+        ltp_bse = kite.ltp(row['bse_instrument_token'])[str(row['bse_instrument_token'])]['last_price']
 
-    if(ltp_nse == ltp_nse):
-        print("=", end="")
-        continue
-    elif(ltp_nse > ltp_bse):
-        print("LTP is higher in NSE is greater than that in BSE")
+        if(ltp_nse == ltp_nse):
+            print("=", end="")
+            continue
+        elif(ltp_nse > ltp_bse):
+            print("LTP is higher in NSE is greater than that in BSE",
+                row['nse_tradingsymbol'], row['nse_tradingsymbol'])
+        
+        else:
+            print("LTP is higher in BSE is greater than that in NSE",
+                row['nse_tradingsymbol'], row['nse_tradingsymbol'])
+        print("Difference: ", ltp_nse - ltp_bse, ltp_nse, ltp_bse)
+        watchlist.append(index)
     
-    else:
-        print("LTP is higher in BSE is greater than that in NSE")
-    print("Difference: ", ltp_nse - ltp_bse, ltp_nse, ltp_bse)
-    watchlist.append(index)
+    except:
+        continue
